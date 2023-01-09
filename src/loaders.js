f4a668a6046508aec3542e43f38f70f0be8fb5b6
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-01-09 08:46:11 -0600

    first commit

---
+++ src/loaders.js
@@ -0,0 +1,15 @@
+// BASE URL OF OUR DEPLOYED API
+// BASE URL OF OUR DEPLOYED API
+const URL = "https://onestbackend.onrender.com"
+
+export const projectsLoader = async () => {
+    const response = await fetch(URL + "/projects")
+    const projects = await response.json()
+    return projects
+}
+
+export const aboutLoader = async () => {
+    const response = await fetch(URL + "/about")
+    const about = await response.json()
+    return about
+}
