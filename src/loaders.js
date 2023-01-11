0137cb3fa72dc2a95637ae98a8a1ee46f3c501f2
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-01-11 10:26:28 -0600

    commit

--- src/loaders.js
+++ src/loaders.js
@@ -1,6 +1,6 @@
 // BASE URL OF OUR DEPLOYED API
 // BASE URL OF OUR DEPLOYED API
-const URL = "https://onestbackend.onrender.com"
+const URL = "http://localhost:4000"
 
 export const projectsLoader = async () => {
     const response = await fetch(URL + "/projects")
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ function Projects(props) {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img style={{height: 600, width:800}}src={project.image} />
         <a href={project.git}>
           <button>Github</button>
         </a>
